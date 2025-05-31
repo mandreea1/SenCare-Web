7851fcfb56ab841309d3729dd6ef0fc44a2cff4d
Andreea Mîțiu <153901400+mandreea1@users.noreply.github.com>
2025-05-31 20:28:03 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -17,6 +17,10 @@ const config = {
   }
 };
 
+console.log('DB_SERVER:', process.env.DB_SERVER);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
+
 async function connectToDb() {
   try {
     await sql.connect(config);
